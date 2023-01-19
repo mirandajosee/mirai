aab2b4fc503251be1a0bbfb0f32151cf0576ee28
José Miranda Valderrama <joseduardomiranda10@gmail.com>
2023-01-18 23:44:28 -0300

    Agregado navegacion y productos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import './components/CartWidget';
-import NavBar from './components/NavBar'
-import ItemListContainer from './components/ItemListContainer'
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar className="NavBar"/>
-      <section className="App-header">
-        <ItemListContainer greeting="Le damos la bienvenida a Mirai, la mejor tienda de ropa"/>
-      </section>
-    </div>
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting="Le damos la bienvenida a Mirai, la mejor tienda de ropa"/>} />
+        <Route path='item/:id' element={<ItemDetailContainer/>} />
+        <Route path='/category/:category' element={<ItemListContainer/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
